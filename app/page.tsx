616e654128b45ee06332bc1f364d0c60d03e8fbf
rubenbarr <benjybeltran1393@gmail.com>
2026-02-17 09:36:04 -0600

    newe changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "./assets/logo_transparent_with_motto1.png"
+import logo from "./assets/logo_transparent_without_motto1.png"
 
 export default function Home() {
   return (
